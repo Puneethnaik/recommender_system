bfd1c4da57591e602de9d6e8661843b114971582
Puneethnaik <puneethn250@gmail.com>
2018-07-27 21:29:38 +0530

    added a few more SQL Calls.

--- routes/api/mySQLCalls.js
+++ routes/api/mySQLCalls.js
@@ -15,6 +15,65 @@ var getMovies = function(callback){
         callback(results);
     })
 }
+
+var getRecommendedMovies = function(movieList, callback){
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'recommender_system'
+    })
+    console.log(movieList)
+    var query = "SELECT * FROM movies WHERE ";
+    for(var i = 0 ; i < movieList.length; i++){
+        var movieID = movieList[i];
+        if(movieID === ''){
+            if(i != movieList.length - 1){
+                if(movieList[i + 1] !== ''){
+    
+                    query += " OR ";
+                }
+            } 
+            continue;   
+        }
+        query += "id = " + movieID;
+        if(i != movieList.length - 1){
+            if(movieList[i + 1] !== ''){
+
+                query += " OR ";
+            }
+        }
+        
+    }
+    console.log(query + "is query");
+    connection.query(query, function(error, results, fields){
+        if(error){
+            console.log(error)
+            return "error"; 
+        }
+        console.log(results)
+        callback(results);
+    })
+}
+function getMyMovies(userID, callback){
+    //this function returns the list of movie objects that the user has rated
+    var query = "SELECT * FROM movies m, ratings r WHERE r.userID = " + userID + " AND m.id = r.movieID";
+    var connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'recommender_system'
+    })
+    connection.query(query, function(error, results, fields){
+        if(error){
+            // console.log(error);
+            return "error"; 
+        }
+        console.log("the results are" + JSON.stringify(results))
+        callback(results);
+    }); 
+}
+
 function addMovieRating(movieID, userID, rating){
     //this function inserts/updates movie rating by a user to a movie
     var query = "SELECT * FROM ratings WHERE movieID = " + movieID + " AND userID = " + userID;
@@ -57,5 +116,7 @@ function addMovieRating(movieID, userID, rating){
 
 module.exports = {
     'getMovies' : getMovies,
-    'addMovieRating' : addMovieRating
+    'addMovieRating' : addMovieRating,
+    'getRecommendedMovies' : getRecommendedMovies,
+    'getMyMovies' : getMyMovies
 }
