c99d98012831359e62bed649192876634247fc5e
Puneethnaik <puneethn250@gmail.com>
2020-06-22 20:39:06 +0530

    Environmentalize MYSQL_HOST, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_USER.

--- mysql-connector-object.js
+++ mysql-connector-object.js
@@ -1,10 +1,10 @@
 //mysql 
 var mysql  = require('mysql');
 var connection = mysql.createConnection({
-    host:'localhost',
-    user:'recommender',
-    password:'',
-    database:'recommender_system',
+    host:process.env['MYSQL_HOST'],
+    user:process.env['MYSQL_USER'],
+    password:process.env['MYSQL_PASSWORD'],
+    database:process.env['MYSQL_DATABASE']
 })
 
 connection.connect(function(err){
--- server.js
+++ server.js
@@ -5,30 +5,10 @@ var login = require('./routes/api/loginAPI');
 var ejs = require('ejs');
 var path = require('path'); 
 var session = require('client-sessions');
-var CronJob = require('cron').CronJob;
 
-
-
-
-
-//
-
-//cron jobs
-/* new CronJob('00 * * * * *', function() {
-     console.log('You will see this message every second');
-    // connection.query("delete from Paste where unix_timestamp(now()) - `expiryDuration` * 60 > `timestamp` / 1000", function(err, results, fields){
-        if(!err){
-            console.log("cron job #1 executed successfully");
-        }
-        else{
-            console.log("contact team! not working");
-        }
-
-    }) 
-}, null, true, 'America/Los_Angeles');
- */
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
@@ -42,7 +22,7 @@ app.use(session({
 
 
 //set your static folder path.
-app.use(express.static(path.join(__dirname, 'public/')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //CORS
 app.use(function(req, res, next){
@@ -54,8 +34,7 @@ app.use(function(req, res, next){
 //---------------Login/Register Routing-------------------------------
 var router = express.Router();
 router.get('/', function(req, res){
-    res.json("Welcome to this time_scheduler web app.");
-    console.log("hello");
+    
 });
 
 router.post('/register', login.register);
@@ -66,12 +45,11 @@ app.use('/api/loginlogout', router);
 
 app.use('/user', require('./routes/user'));
 
-//recommender system apis
+// recommender system apis
 // app.use('/recommender', require('./routes/api/recommenderAPI'))
 
 app.get('/', function(req, res){
-    console.log("index page");
-    res.end("Hello");
+    res.end("Not Allowed");
 });
 
 app.get('/login', function(req, res){
@@ -88,11 +66,11 @@ app.get('/profile', function(req, res){
         res.render('dashboard.ejs', {title:'Dashboard', user : req.session.user});
 });
 
-
 app.get('/logout', function(req, res){
     req.session.user = undefined;
     res.redirect('/profile');
 })
+
 app.listen(3000, function(req, res){
     console.log("Server connected and serving at port 3000");
 });
